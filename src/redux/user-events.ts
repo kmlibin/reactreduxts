20b667dd781aea37de6dd31388e9d2ad2ef8e101
Kelli Libin <km.putnam2@gmail.com>
2023-02-15 15:25:55 -0700

    added thunks and loading events

--- src/redux/user-events.ts
+++ src/redux/user-events.ts
@@ -1,4 +1,6 @@
-import { AnyAction } from 'redux';
+import { Action, AnyAction } from 'redux';
+import { ThunkAction } from 'redux-thunk/es/types';
+import { RootState } from './store';
 
 interface UserEvent {
   id: number;
@@ -12,14 +14,73 @@ interface UserEventsState {
   allIds: UserEvent['id'][];
 }
 
+//action types
+const LOAD_REQUEST = 'userEvents/load_request';
+const LOAD_SUCCESS = 'userEvents/load_success';
+const LOAD_FAILURE = 'userEvents/load_failure';
 
-const initialState: UserEventsState = {
-    byIds: {},
-    allIds: []
+//describe action objects
+interface LoadRequestAction extends Action<typeof LOAD_REQUEST> {}
+
+interface LoadSuccessAction extends Action<typeof LOAD_SUCCESS> {
+  payload: {
+    events: UserEvent[];
+  };
 }
 
-const userEventsReducer = (state: UserEventsState = initialState, action: AnyAction) => {
+interface LoadFailureAction extends Action<typeof LOAD_FAILURE> {
+  error: string;
+}
+
+//action creator..function that returns a thunk action function
+
+export const loadUserEvents =
+  (): ThunkAction<
+    void,
+    RootState,
+    undefined,
+    LoadRequestAction | LoadSuccessAction | LoadFailureAction
+  > =>
+  async (dispatch, getState) => {
+    dispatch({
+      type: LOAD_REQUEST,
+    });
+
+    try {
+      const response = await fetch('http://localhost:3001/events');
+      const events: UserEvent[] = await response.json();
+      dispatch({
+        type: LOAD_SUCCESS,
+        payload: { events },
+      });
+    } catch (e) {
+      dispatch({
+        type: LOAD_FAILURE,
+        error: 'Failed to load events',
+      });
+    }
+  };
+
+const initialState: UserEventsState = {
+  byIds: {},
+  allIds: [],
+};
+
+const userEventsReducer = (
+  state: UserEventsState = initialState,
+  action: LoadSuccessAction
+) => {
   switch (action.type) {
+    case LOAD_SUCCESS:
+      const { events } = action.payload;
+      return {
+        ...state,
+        allIDs: events.map(({ id }) => id),
+        byIds: events.reduce<UserEventsState['byIds']>((byIds, event) => {
+          byIds[event.id] = event;
+          return byIds;
+        }, {}),
+      };
     default:
       return state;
   }
